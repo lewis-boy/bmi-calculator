e2783390bf4952164895ea4cf3d347bfbd29de66
Luis Escobar <luisaeniebla@gmail.com>
2023-08-16 15:56:36 -0700

    half-way thru the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import React, { FC } from 'react'
 
 interface HeroProps {
 
@@ -6,8 +6,19 @@ interface HeroProps {
 
 const Hero: FC<HeroProps> = ({ }) => {
     return (
-        <div>
-            Hero
+        <div className="w-full flex mb-[65px] relative">
+            <div className="w-[1208px] h-[737px] bg-gradient-to-br from-transparent to-gradient-blue rounded-b-[35px] pl-[116px] pr-[300px]">
+                <img src='/images/logo.svg' alt='logo' className="mt-[75px] mb-[127px]" />
+                <div className="w-[485px] flex flex-col items-start">
+                    <h1 className="mb-[35px] text-[64px] text-gunmetal font-semibold leading-[110%]">Body Mass Index Calculator</h1>
+                    <p className="text-[16px] leading-[150%] text-dark-electric-blue">Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
+                </div>
+            </div>
+            <div className="w-[664px] h-[484px] p-[32px] absolute top-[106px] right-[300px] bg-white rounded-[16px]
+                shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]
+            ">
+
+            </div>
         </div>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors:{
-        "blue": 'hsl(227, 92%, 58%)',
-        "gunmetal": 'hsl(215, 31%, 21%)',
-        "dark-electric-blue": 'hsl(215, 17%, 45%)',
-        'border-color': 'hsl(200, 24%, 88%)',
+      colors: {
+        blue: "hsl(227, 92%, 58%)",
+        gunmetal: "hsl(215, 31%, 21%)",
+        "dark-electric-blue": "hsl(215, 17%, 45%)",
+        "border-color": "hsl(200, 24%, 88%)",
+        "gradient-blue": "hsl(216, 95%, 92%)",
       },
-      fontFamily:{
-        inter: ["--font-inter"]
-      }
+      fontFamily: {
+        inter: ["--font-inter"],
+      },
     },
   },
   plugins: [],
